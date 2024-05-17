004d042c89455dba1a0e8a001a7985454f7a7584
Rafael Paniago <115939524+rafaelpaniago@users.noreply.github.com>
2024-05-16 22:28:12 -0300

    Criando arquivos para o BLOG
    Htmls criados para o blog. Páginas de blog e post, bem como ajuste no arquivo views e urls.

--- projeto_site/blog/views.py
+++ projeto_site/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def blog(request):
+    return render(request, 'blog/blog.html')
+
+def post(request):
+    return render(request, 'blog/post.html')
