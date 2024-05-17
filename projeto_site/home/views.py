164cf2afe340c0367efcb44fb33725dfc17e8a7f
Rafael Paniago <115939524+rafaelpaniago@users.noreply.github.com>
2024-05-16 22:16:58 -0300

    Atualizações pastas app
    Criação arquivo urls.py da pasta do app "home". Criação do arquivo urls.py da pasta do app "blog". Criação dos arquivos html (index.html, contato.html).
    
    Vinculação do arquivo views com URLS e templates html.

--- projeto_site/home/views.py
+++ projeto_site/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home/index.html')
+
+def contato(request):
+    return render(request, 'home/contato.html')
